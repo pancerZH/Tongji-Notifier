2b0a5003977989a15026d3f292b7a0d5ccc51e29
ZWZ <942740938@qq.com>
2018-03-19 16:50:12 +0800

    增加了now模式

--- run.py
+++ run.py
@@ -141,6 +141,7 @@ def send_to_user(note_dict, detail_dict):
 
 
 def deploy():
+    """ 部署模式，不发送新通知，仅发送部署通知邮件 """
     print('deploying')
     header={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
@@ -169,7 +170,34 @@ def deploy():
     print('deploy succeed!')
 
 
+def run_now():
+    """ 立刻检查4m3并将新通知通过邮件发送，仅执行一次 """
+    header={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
+
+    if os.path.exists('notifier.log') == False:
+        env_build.create_log()
+    if os.path.exists('TJ_notice.db') == False:
+        env_build.create_database()
+    if os.path.exists('TJ') == False:
+        env_build.create_TJID()
+    if os.path.exists('mail') == False:
+        env_build.create_mail()
+
+    s=requests.session()
+    try:
+        login(header,s)
+    except:
+        env_build.write_to_log('failed to log in 4m3')
+        exit(3)
+    note_dict = get_table(header,s)
+    notice_list = act_with_database(note_dict)
+    detail_dict = get_detail(header,s,notice_list)
+    send_to_user(note_dict,detail_dict)
+
+
 def run_service():
+    """ 正常运行模式 """
     header={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 
@@ -203,6 +231,9 @@ def run_service():
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'deploy':
         deploy()
+        run_service()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'now':
+        run_now()
         run_service()
     else:
         run_service()
